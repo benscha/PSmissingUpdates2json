3c4ea3fdf418bb99598e13ee745e993fd72f9002
benscha <38983142+benscha@users.noreply.github.com>
2021-05-06 16:51:24 +0200

    Update missingUpdates.ps1

--- missingUpdates.ps1
+++ missingUpdates.ps1
@@ -1,20 +1,15 @@
 #Global
-$hostname = [System.Net.Dns]::GetHostByName($env:computerName) | Select -ExpandProperty Hostname | Out-String
-$LastUpdate = get-Hotfix| select InstalledON  | sort { [datetime]$_.InstalledON  } -desc| Select-Object -First 1 
-$LogDate = (Get-Date -Format "yyyy.MM.dd_HH_mm")
-$LastBootTime = Get-WmiObject win32_operatingsystem | select @{LABEL=’LastBootUpTime’;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
-$hostinfo = [pscustomobject]@{'hostname'=$hostname;'LastUpdated'= $LastUpdate.InstalledOn;'LogDate'= (Get-Date -Format "dd.MM.yyyy HH:mm");'LastBoot'=$LastBootTime.lastbootuptime}
-$FileName = $env:computerName +"_" + $LogDate.ToString() + ".json" 
-
-
-
+$hostname = [System.Net.Dns]::GetHostByName($env:computerName) | Select -ExpandProperty Hostname
+$LastUpdate = Get-Date -Format "yyyy.MM.dd HH:mm:ss" (get-Hotfix | select InstalledON  | sort { [datetime]$_.InstalledON  } -desc| Select-Object -ExpandProperty InstalledOn -First 1)
+$LogDate = (Get-Date -Format "yyyy.MM.dd HH:mm:ss")
+$LastBootTime = Get-Date -Format "yyyy.MM.dd HH:mm:ss" (Get-WmiObject win32_operatingsystem | select @{LABEL=’LastBootUpTime’;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} | Select-Object -ExpandProperty LastBootUpTime )
+$os = (Get-WmiObject -class Win32_OperatingSystem).Caption
+$hostinfo = [pscustomobject]@{'hostname'=$hostname;'LastUpdated'= $LastUpdate;'LogDate'= (Get-Date -Format "yyyy.MM.dd HH:mm:ss");'LastBoot'=$LastBootTime;'os'=$os}
 
 #Collect RAW Data
 $UpdateSession = New-Object -ComObject Microsoft.Update.Session
 $UpdateSearcher = $UpdateSession.CreateupdateSearcher()
 $Updates1 = @($UpdateSearcher.Search("IsHidden=0 and IsInstalled=0").Updates)
-$Updates1 | Select-Object *
-
 
 #Prepare Data for Arra<y
 $UpdTypes = $Updates1.Categories.Name.Split(“`n”) 
@@ -22,25 +17,25 @@ $UpdTitle = $Updates1.Title.Split(“`n”)
 $UpdKB = $Updates1.KBArticleIDs
 $UpdRelDate = $Updates1.LastDeploymentChangeTime
 
-
 # Combine Arrays
 [int]$max = $UpdTitle.Count
-if ([int]$UpdTypes.count -gt [int]$UpdTitle.count) { $max = $UpdTypes.Count; }
- 
-$ResultsAll = for ( $i = 0; $i -lt $max; $i++)
-{
-    Write-Verbose "$($UpdTypes[$i]),$($UpdTitle[$i]),$($UpdKB[$i]),$($UpdRelDate[$i])"
-    [PSCustomObject]@{
-        UpdateType = $UpdTypes[$i]
-        Title = $UpdTitle[$i]
-        KBNo = $UpdKB[$i]
-        ReleaseDate = $UpdRelDate[$i]      
-    }
-}
-$Results = $ResultsAll | Where-Object {$_.UpdateType -ne "Drivers"} 
 
+$ResultsAll = @()
+
+foreach ($update in $Updates1) {
+    $ResultsAll += (
+        [PSCustomObject]@{
+            UpdateType = $update.Categories[0].Name
+            Title = $update.Title
+            KBNo = $update.KBArticleIDs[0]
+            ReleaseDate = Get-Date -Format "yyyy.MM.dd HH:mm:ss" $update.LastDeploymentChangeTime
+            }
+        )
+}
+
+$Results = $ResultsAll | Where-Object {$_.UpdateType -ne "Drivers"} 
 
 $jsonBase = @{}
 $jsonBase.Add("Data",$Results)
 $jsonBase.Add("Hostinfo",$hostinfo)
-$jsonBase | ConvertTo-Json -Depth 10 | Out-File $FileName
+$jsonBase = $jsonBase | ConvertTo-Json
